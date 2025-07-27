34d099a59ca73a7598cd13e28d8c94e18cbdaac9
vadimzakirov <vadimzakiroff@gmail.com>
2025-07-28 00:39:27 +0500

    First commit

---
+++ src/main/java/ru/vadim/springsecsection1/EasyBankBackendApplication.java
@@ -0,0 +1,15 @@
+package ru.vadim.springsecsection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBankBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyBankBackendApplication.class, args);
+    }
+
+}
+
+
---
+++ src/test/java/ru/vadim/springsecsection1/EasyBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vadim.springsecsection1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBankBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
